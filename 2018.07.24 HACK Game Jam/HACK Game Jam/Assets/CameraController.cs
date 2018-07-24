c18085ca1b034036c017439d057ec02adcef48e3
Unknown <mvhaydon@gmail.com>
2018-07-24 15:26:14 +1200

    Camera Controlls

---
+++ 2018.07.24 HACK Game Jam/HACK Game Jam/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector3 pos;
+    float edgeSize = 50f;
+    float speed = 20f;
+    // Use this for initialization
+    void Start () {
+        pos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.mousePosition.x > Screen.width - edgeSize)
+            {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x < edgeSize)
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y < edgeSize)
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
